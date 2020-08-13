41732734921415dc2404db537fbb83aa7df83fb4
yynespoon <39826815+yynespoon@users.noreply.github.com>
2020-08-14 02:23:12 +0800

    add healthchek and chat test

---
+++ netty-test/src/main/java/com/test/TestMain.java
@@ -0,0 +1,13 @@
+package com.test;
+
+/**
+ * @author lixiaoyu
+ * @since 2020-08-13 17:29
+ */
+public class TestMain {
+
+    public static void main(String[] args) {
+        System.out.println(Integer.toBinaryString(-1));
+        System.out.println(Integer.toBinaryString(1));
+    }
+}
